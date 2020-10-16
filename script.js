1c9eae5604d798ded87f4ec509a6a71e69ac016f
Michael Krumland-Dunning <60492174+Michael-KD@users.noreply.github.com>
2020-10-15 17:09:10 -0700

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,11 @@
+$(".graph").hide();
+
+$(".one").click(function() {
+    $(".world").hide();
+    $(".graph").show();
+});
+
+$(".two").click(function() {
+    $(".graph").hide();
+    $(".world").show();
+});
